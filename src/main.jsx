dc50bac84bdb8f5a67b89ee20a06d749cb102b73
jserrano <jserrano@procesos-eficientes.com>
2025-08-15 10:05:47 -0500

    Se modifico el main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { GifExpertApp } from './GifExpertApp'
 
 import './styles.css'
-console.log('API key desde env:', import.meta.env.VITE_API_KEY);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>{/**el StricMode es una herramienta de desarrollo que envuelve tu aplicación para ayudarte a encontrar errores y malas prácticas antes de ir a producción  */}
     <GifExpertApp />
