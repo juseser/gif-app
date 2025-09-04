dd1d91cb7572eef6716dee28de39f202625f2a73
jserrano <jserrano@procesos-eficientes.com>
2025-09-03 21:39:21 -0500

    Se cambio la identacion

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import { AddCategory, GifGrid } from './components/';
 
-
 export const GifExpertApp = () => {
-
-
+    
     const [ categorias , setCategorias ] = useState([ 'Batman' ]);//se inicializa el useState categorias como un arreglo
 
     //Funcion para agregar una nueva categoria de gifs, recibe como argumento newCategory, el cual viene de AddCategory.jsx
@@ -13,21 +11,20 @@ export const GifExpertApp = () => {
         setCategorias( [ ...categorias, newCategory ] );
     }
     
+    return (
+        <>
+            
+            <h1>GifExpertApp</h1>
 
-  return (
-    <>
-        
-        <h1>GifExpertApp</h1>
-
-        <AddCategory onNewCategory={ (value) => onAddCategory(value) }/>
-        
-        {/* Se pone entre llaves porque es una expresion de javascript */}
-        { 
-            categorias.map( ( categoria )=>(
-                <GifGrid  key={ categoria } categoria={ categoria }/>//React crea una instancia del componente GifGrid y la muéstra aquí
-            ))//se ponen parentesis para no poner el return
-        }
-        
-    </>
-  )
+            <AddCategory onNewCategory={ (value) => onAddCategory(value) }/>
+            
+            {/* Se pone entre llaves porque es una expresion de javascript */}
+            { 
+                categorias.map( ( categoria )=>(
+                    <GifGrid  key={ categoria } categoria={ categoria }/>//React crea una instancia del componente GifGrid y la muéstra aquí
+                ))//se ponen parentesis para no poner el return
+            }
+            
+        </>
+    )
 }
